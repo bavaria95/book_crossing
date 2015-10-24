c3bca93b198cbfe88a70c37ea99d5bdb97b5e53b
Dima Boyko <dimka.boyko@hotmail.com>
2015-10-24 03:43:33 +0200

    edited book model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151024012218) do
+ActiveRecord::Schema.define(version: 20151024014028) do
+
+  create_table "books", force: :cascade do |t|
+    t.string   "title"
+    t.text     "descriprion"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.integer  "user_id"
+  end
+
+  add_index "books", ["user_id"], name: "index_books_on_user_id"
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
