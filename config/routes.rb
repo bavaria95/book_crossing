f152082312168e280d56bc148b26b06018b36bf5
Dmitry Petruk <bavaria95@gmail.com>
2015-10-24 05:56:26 +0200

    added books link for users

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,14 @@
 Rails.application.routes.draw do
   devise_for :users
+  root "home#index"
+  
+  get "home/index"
+  
   get 'users/profile', as: 'user_root'
   get 'users/profile'
 
-  get "home/index"
-  root "home#index"
+  get 'users/books' 
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
