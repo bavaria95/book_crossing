ac0883bd344a2543ef51522f2aeb3489b371a320
Dmitry Petruk <bavaria95@gmail.com>
2015-10-24 17:06:24 +0200

    added possibility to alter own city

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,7 +9,7 @@ class ApplicationController < ActionController::Base
 
         def configure_permitted_parameters
             devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:name, :email, :password,:city) }
-            devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:name, :email, :password, :current_password, :is_female, :date_of_birth) }
+            devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:name, :city, :email, :password, :current_password, :is_female, :date_of_birth) }
         end
 	def cities
 		f = File.readlines("/public/cities.dat")
