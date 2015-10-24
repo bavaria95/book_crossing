9439bff2977b5a33cb48404420678d4846bcd477
Dima Boyko <dimka.boyko@hotmail.com>
2015-10-24 08:57:38 +0200

    added Edit page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,4 +5,7 @@ class UsersController < ApplicationController
   def books
   	@books = Book.where(user_id: current_user.id)
   end
+  def groups
+  	@close_users = User.where(city: current_user.city)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,8 @@ Rails.application.routes.draw do
   get 'users/profile'
 
   get 'users/books' 
+  get 'users/groups'
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
